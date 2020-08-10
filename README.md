ca1174566a554138e27c6412285cde2626a747eb
romanguerrero <40374453+romanguerrero@users.noreply.github.com>
2020-08-10 14:16:46 -0700

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tetris
+Tetris implemented in Python
+
+Tutorial by Tech with Tim
+Link: https://techwithtim.net/tutorials/game-development-with-python/tetris-pygame/tutorial-1/
---
+++ tetris.py
@@ -0,0 +1,14 @@
+# file: tetris.py
+# auth: Roman Guerrero
+# date: 20/08/10
+# desc: Simple Tetris game implemented in Python
+# cita: Tech with Tim https://techwithtim.net/tutorials/game-development-with-python/tetris-pygame/tutorial-1/
+
+
+import pygame
+import random
+
+
+
+if __name__ == '__main__':
+    dummy = 0
